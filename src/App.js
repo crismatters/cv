ff955f01cd555cf3f6482d66050832cfb259d01c
crismatters <crismatters@outlook.com>
2021-07-17 18:17:40 -0500

    [add] firebase

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
               description: 'Google Cloud Platform Support Engineer.',
               companyWebSite: 'https://about.google/',
               datesBetween: '2021.07 - Present',
-              descriptionTags: ["Google Cloud"]
+              //  descriptionTags: ["Google Cloud"]
             },
             {
               title: 'DevOps Engineer',
@@ -39,7 +39,7 @@ const App = () => {
               companyWebSite: 'https://www.softtek.com/',
               companyMeta: '',
               datesBetween: '2019.12 - 2021.07',
-              descriptionTags: ["Ansible", "SaltStack", "Linux", "SCM", "GitlabCI", "Kubernetes", "Azure", "NodeJS", "MongoDB", "ReactJS", "SAFe DevOps"]
+              //   descriptionTags: ["Ansible", "SaltStack", "Linux", "SCM", "GitlabCI", "Kubernetes", "Azure", "NodeJS", "MongoDB", "ReactJS", "SAFe DevOps"]
             },
           ]
         },
@@ -75,21 +75,65 @@ const App = () => {
           icon: 'book',
           items: [
             {
-              title: 'SaltStack Certified Engineer / 2020',
+              title: 'A Beginner\'s Guide to Linux Kernel Development (LFD103)',
+              authority: 'The Linux Foundation',
+              authorityWebSite: 'https://www.linuxfoundation.org/',
+              rightSide: '2021.05'
+            },
+            {
+              title: 'Cyber Security Foundation Professional Certificate (CSFPC)',
+              authority: 'CertiProf',
+              authorityWebSite: 'https://certiprof.com/',
+              rightSide: '2021.05'
+            },
+            {
+              title: 'Scrum Foundation Professional Certificate (SFPC)',
+              authority: 'CertiProf',
+              authorityWebSite: 'https://certiprof.com/',
+              rightSide: '2021.05'
+            },
+            {
+              title: 'Certified Kubernetes Administrator',
               authority: 'Linux Academy',
-              authorityWebSite: 'https://app.linuxacademy.com'
+              authorityWebSite: 'https://app.linuxacademy.com',
+              rightSide: '2020.11'
             },
             {
-              title: 'Some Conferences / 2019',
-              authority: 'SomeConf',
-              authorityMeta: 'Speaker',
-              authorityWebSite: 'https://www.someconf.somesome',
-              rightSide: 'test'
+              title: 'RedHat Certified Specialist in Ansible Automation',
+              authority: 'Linux Academy',
+              authorityWebSite: 'https://app.linuxacademy.com',
+              rightSide: '2020.08'
             },
             {
-              title: 'Some Conferences / 2012',
-              authorityMeta: 'Speaker'
-            }
+              title: 'RedHat Certified Engineer',
+              authority: 'Linux Academy',
+              authorityWebSite: 'https://app.linuxacademy.com',
+              rightSide: '2020.07'
+            },
+            {
+              title: 'SaltStack Certified Engineer',
+              authority: 'Linux Academy',
+              authorityWebSite: 'https://app.linuxacademy.com',
+              rightSide: '2020.02'
+            },
+            {
+              title: 'Partner: NDG Linux Unhatched',
+              authority: 'Cisco Networking Academy',
+              authorityWebSite: 'https://netacad.com',
+              rightSide: '2019.08'
+            },
+            {
+              title: 'CCNA Routing & Switching',
+              authority: 'Cisco Networking Academy',
+              authorityWebSite: 'https://netacad.com',
+              rightSide: '2019.06'
+            },
+            {
+              title: 'Introduction to Cibersecurity',
+              authority: 'Cisco Networking Academy',
+              authorityWebSite: 'https://netacad.com',
+              rightSide: '2019.05'
+            },
           ]
         },
         {
@@ -109,6 +153,16 @@ const App = () => {
               authority: 'German',
               authorityMeta: 'Beginner'
             },
+          ]
+        },
+        {
+          type: 'tag-list',
+          title: 'Skills Proficiency',
+          icon: 'rocket',
+          items: [
+            "Linux", "Windows", "Wireshark", "NMap", "MySQL", "PostgreSQL", "MongoDB", "JavaScript",
+            "RedHat Ansible", "SaltStack", "Kubernetes", "GitlabCI", "Docker", "Scrum", "Google Cloud", "Azure",
+            "Jira", "ReactJS", "Agile", "Scrum", "SAFe DevOps"
           ]
         },
       ]}
