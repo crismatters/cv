b4658e566dac2bdf9bb4405dfc7eda66fb3b6280
crismatters <crismatters@github.com>
2024-01-15 23:10:18 -0600

    [Add] Terminal animation

--- src/components/shared/AppBanner.js
+++ src/components/shared/AppBanner.js
@@ -1,13 +1,18 @@
-import useThemeSwitcher from '../../hooks/useThemeSwitcher';
+//import useThemeSwitcher from '../../hooks/useThemeSwitcher';
 import { FiArrowDownCircle } from 'react-icons/fi';
-import developerLight from '../../images/developer.svg';
-import developerDark from '../../images/developer-dark.svg';
 import { motion } from 'framer-motion';
 import Data from '../../data/Data';
+import Terminal from 'react-animated-term'
+import 'react-animated-term/dist/react-animated-term.css'
 
 const AppBanner = () => {
-	const [activeTheme] = useThemeSwitcher();
-
+	//const [activeTheme] = useThemeSwitcher();
+	const termLines = [
+		{ text: "id", cmd:true },
+		{ text: "uid=1000(crismatters) Systems Engineer", cmd:false },
+		{ text: "uid=1000(crismatters) Web Design Enthusiast", cmd:false },
+		{ text: "uid=1000(crismatters) Cloud Security Enthusiast", cmd:false },
+	]
 	return (
 		<motion.section
 			initial={{ opacity: 0 }}
@@ -38,7 +43,6 @@ const AppBanner = () => {
 					}}
 					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
-					{Data.salutation.description}
 				</motion.p>
 				<motion.div
 					initial={{ opacity: 0 }}
@@ -69,11 +73,10 @@ const AppBanner = () => {
 				transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
 				className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
 			>
-				<img
-					src={
-						activeTheme === 'dark' ? developerLight : developerDark
-					}
-					alt="Developer"
+				<Terminal
+					lines={termLines}
+					interval={600}
+					delay={{start: 1200, characters: 90, lines: 1000}} 
 				/>
 			</motion.div>
 		</motion.section>
